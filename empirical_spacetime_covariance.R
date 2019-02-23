6c3e919d7fc5fac4e4ff5b252adff34519540f08
marysalvana <mary.salvana@obf.ateneo.edu>
2019-02-23 16:29:40 +0300

    Create empirical_spacetime_covariance.R

---
+++ empirical_spacetime_covariance.R
@@ -0,0 +1,86 @@
+empirical_st_cov <- function( data1, data2 = NULL, cross, locations, max_time_lag ){
+  
+  # input: data matrix with temporal replicates as rows and each column represents the locations
+  # cross: TRUE or FALSE
+  
+  # output: correlation
+  
+  mean = colMeans(data1)
+  scov = matrix(c(0), ncol(data1), ncol(data1))
+  n_stations = ncol(data1)
+  
+  emp <- list()
+  
+  if(!cross == FALSE){
+    
+    for(tau in 0:max_time_lag){
+      
+      for(i in (tau+1):(nrow(data)-tau)){
+        scov = scov + (data1[i,] - mean)%*%t(data1[i+tau,] - mean)
+      }
+      
+      scov = scov/(nrow(data1) - tau)
+      
+      sd = apply(data1, 2, sd)
+      
+      Dz = sd*diag(n_stations)
+      
+      scor = solve(Dz)%*%scov%*%solve(Dz)
+      
+      ## Arrange the empirical covariance values in one dataframe/matrix. 
+      
+      loc <- 1
+      xlag <- locations[loc,1]-locations[,1]
+      ylag <- locations[loc,2]-locations[,2]
+      emp_vals <- scor[,loc]
+      
+      for(loc in 2:ncol(data1)){
+        xlag <- c(xlag, locations[loc,1] - locations[,1])
+        ylag <- c(ylag, locations[loc,2] - locations[,2])
+        emp_vals <- c(emp_vals,scor[,loc])
+      }
+      emp_temp <- data.frame(xlag,ylag,emp_vals)
+      colnames(emp_temp) <- c('xlag', 'ylag', 'correlation')
+      emp[[tau+1]] <- emp_temp
+    }
+    
+  }else{
+    
+    for(tau in 0:max_time_lag){
+      
+      mean2 = colMeans(data2)
+      
+      for(i in (tau+1):(nrow(data1)-tau)){
+        scov = scov + (data1[i,] - mean)%*%t(data2[i+tau,] - mean2)
+        
+      }
+      scov = scov/(nrow(data1) - tau)
+      
+      sd = apply(data1, 2, sd)
+      sd2 = apply(data2, 2, sd)
+      
+      Dz = sd*diag(n_stations)
+      Dz2 = sd2*diag(n_stations)
+      
+      scor = solve(Dz)%*%scov%*%solve(Dz2)
+      
+      ## Arrange the empirical covariance values in one dataframe/matrix. 
+      
+      loc <- 1
+      xlag <- locations[loc,1]-locations[,1]
+      ylag <- locations[loc,2]-locations[,2]
+      emp_vals <- scor[,loc]
+      
+      for(loc in 2:ncol(data)){
+        xlag <- c(xlag, locations[loc,1] - locations[,1])
+        ylag <- c(ylag, locations[loc,2] - locations[,2])
+        emp_vals <- c(emp_vals,scor[,loc])
+      }
+      emp_temp <- data.frame(xlag,ylag,emp_vals)
+      colnames(emp_temp) <- c('xlag', 'ylag', 'correlation')
+      emp[[tau+1]] <- emp_temp
+    }
+  }
+  
+  return(emp)
+}
